2953e787bc97145430ba25aa0f5c09b84bec9266
niazulalam <niazulalam097@gmail.com>
2025-03-08 11:33:16 +0600

    back-to top button added

--- js/script.js
+++ js/script.js
@@ -75,4 +75,30 @@ $('.sliders').slick({
     dots: true,
     prevArrow: '<i class="fa-solid fa-circle-arrow-left leftArrow"></i>',
     nextArrow: '<i class="fa-solid fa-circle-arrow-right rightArrow"></i>',
-  })
+  });
+
+
+  //==== Back-to-top button
+let backToTop = $(".back-to-top");
+backToTop.hide();
+$(window).on("scroll", function (event) {
+if ($(this).scrollTop() > 600) {
+  $(".back-to-top").fadeIn(200);
+} else {
+  $(".back-to-top").fadeOut(200);
+}
+});
+//==== Animate the scroll to top
+$(".back-to-top").on("click", function (event) {
+event.preventDefault();
+
+$("html, body").animate(
+  {
+    scrollTop: 0,
+  },
+  1000
+);
+});
+
+
+
